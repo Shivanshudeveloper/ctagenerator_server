5f81b136a8081026a0301b0d58d680840ec38b32
shiv <developer.shivanshu@gmail.com>
2024-12-24 17:14:09 +0530

    Work

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ const APP_MAIN_URL = "https://app.seefunnel.com";
 // const OTHER_SERVICE_URL = "http://localhost:5050";
 const OTHER_SERVICE_URL = "https://airesourcessortwindwork-bseud6bndzehgjg9.eastus-01.azurewebsites.net";
 
-const CALLING_SERVICE_URL = "https://aicalling-ckc7gadma6cmbcda.canadacentral-01.azurewebsites.net";
+const CALLING_SERVICE_URL = "https://ctacallingservice-fqf6g5bdguazefc5.eastus-01.azurewebsites.net";
 
 
 
